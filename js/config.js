d3a45971172f464100af2f0ffbb9ae760d157cf8
adamj <a.j.jones@sky.com>
2024-05-18 22:36:12 +0100

    updated config for testing/github compatibility

--- js/config.js
+++ js/config.js
@@ -1,4 +1,10 @@
 const config = {
     clientId: '44908711b91a459e90eb7ac1b4c2b8da',
-    redirectUri: 'https://a-j-jones.github.io/festival_spotify/callback'
-};
+    get redirectUri() {
+      if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        return 'http://localhost:8000/callback.html';
+      } else {
+        return 'https://a-j-jones.github.io/festival_spotify/callback';
+      }
+    }
+  };
