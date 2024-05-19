99b0b6c83e663a71f01b1ea461a769ae14a2a2a9
adamj <a.j.jones@sky.com>
2024-05-19 01:15:03 +0100

    Save access token to avoid re-authorisation

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ const artistList = document.getElementById('artist-list');
 
 const spotifyApi = new SpotifyWebApi();
 
+const storedAccessToken = localStorage.getItem('accessToken');
+if (storedAccessToken) {
+  spotifyApi.setAccessToken(storedAccessToken);
+  getMatchingArtists();
+} else {
+  loginButton.hidden = false;
+}
+
 loginButton.addEventListener('click', () => {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${config.clientId}&response_type=token&redirect_uri=${encodeURIComponent(config.redirectUri)}&scope=user-library-read`;
   window.open(authUrl, '_blank', 'width=600,height=800');
@@ -13,6 +21,10 @@ window.addEventListener('message', event => {
   if (event.data.type === 'access_token') {
     const accessToken = event.data.accessToken;
     spotifyApi.setAccessToken(accessToken);
+    
+    localStorage.setItem('accessToken', accessToken);
+    loginButton.hidden = true;
+
     getMatchingArtists();
   }
 });
